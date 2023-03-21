00856fac92d2b90798f1683072d0addd896db92e
badgersky <szymonek.ptasznik@gmail.com>
2023-03-21 21:51:25 +0100

    display generated password functionality

--- app.py
+++ app.py
@@ -20,14 +20,19 @@ class App:
             )
         self.generate_password_button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
-    def generate_password(self):
+        self.textbox = customtkinter.CTkTextbox(self.root, width=200, height=20)
+        self.textbox.pack(pady=150, padx=0)
+
+    @staticmethod
+    def generate_password():
         special_characters = '!@#$%^&*()_-<>?'
         all_characters = ascii_lowercase + ascii_uppercase + digits + special_characters
         return ''.join(choice(all_characters) for _ in range(15))
 
     def display_password(self):
+        self.textbox.delete('0.0', 'end')
         password = self.generate_password()
-        print(password)
+        self.textbox.insert('0.0', f'{password}')
 
     def run_app(self):
         self.root.mainloop()
