b9281430264287321988e5f3fb6765d46b8f4c34
badgersky <szymonek.ptasznik@gmail.com>
2023-03-22 19:14:17 +0100

    corrected mistakes

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 from cryptography.fernet import Fernet
 
 
-def encrypt(password):
+def encrypt_password(password):
     key = Fernet.generate_key()
     fernet = Fernet(key)
-    dec_password = fernet.decrypt(password.encode())
+    dec_password = fernet.encrypt(password.encode())
     return key + dec_password
 
 
-def decrypt(dec_password):
+def decrypt_password(dec_password):
     key = dec_password[:44]
     fernet = Fernet(key)
     dec_password = dec_password[44:]
