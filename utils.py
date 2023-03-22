0635615e05366c813e72a7dbbcbbaa1d9f1de8a4
badgersky <szymonek.ptasznik@gmail.com>
2023-03-22 19:10:08 +0100

    created encrypting password functionality

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 from cryptography.fernet import Fernet
 
 
-def encrypt():
-    pass
+def encrypt(password):
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    dec_password = fernet.decrypt(password.encode())
+    return key + dec_password
 
 
-def decrypt():
+def decrypt(dec_password):
     pass
