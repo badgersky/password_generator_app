f9974732e111efa16f78c6f4f6b2b0fadd5e2af5
badgersky <szymonek.ptasznik@gmail.com>
2023-03-22 13:41:43 +0100

    located textboxes and button on screen using grid

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
 import tkinter
-import customtkinter
+import customtkinter as ctk
 from random import choice
 from string import ascii_lowercase, ascii_uppercase, digits
 
 
-class App:
+class App(ctk.CTk):
 
     def __init__(self):
-        customtkinter.set_appearance_mode('System')
-        customtkinter.set_default_color_theme('green')
+        super().__init__()
+        ctk.set_appearance_mode('System')
+        ctk.set_default_color_theme('green')
 
-        self.root = customtkinter.CTk()
-        self.root.geometry('500x500')
+        self.geometry('400x200')
+        self.title('PasswordGenerator')
 
-        self.generate_password_button = customtkinter.CTkButton(
-            master=self.root,
-            text='GeneratePassword',
-            command=self.display_password
-            )
-        self.generate_password_button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=2)
 
-        self.textbox = customtkinter.CTkTextbox(self.root, width=200, height=20)
-        self.textbox.pack(pady=150, padx=0)
+        # password
+        self.password_label = ctk.CTkLabel(self, text='Password:')
+        self.password_label.grid(column=0, row=0, padx=20, pady=10, sticky='w')
+
+        self.password_textbox = ctk.CTkTextbox(self, height=40)
+        self.password_textbox.grid(column=1, row=0, padx=20, pady=10, sticky='nsew')
+
+        # password to what
+        self.to_what_label = ctk.CTkLabel(self, text='Password to what?:')
+        self.to_what_label.grid(column=0, row=1, padx=20, pady=10, sticky='w')
+
+        self.to_what_textbox = ctk.CTkTextbox(self, height=40)
+        self.to_what_textbox.grid(column=1, row=1, padx=20, pady=10, sticky='nsew')
+
+        # generate password button
+        self.generate_password_button = ctk.CTkButton(self, text='GeneratePassword', command=self.display_password)
+        self.generate_password_button.grid(column=0, row=2, padx=20, pady=10, sticky='w')
 
     @staticmethod
     def generate_password():
@@ -30,12 +42,12 @@ class App:
         return ''.join(choice(all_characters) for _ in range(15))
 
     def display_password(self):
-        self.textbox.delete('0.0', 'end')
+        self.password_textbox.delete('0.0', 'end')
         password = self.generate_password()
-        self.textbox.insert('0.0', f'{password}')
+        self.password_textbox.insert('0.0', f'{password}')
 
     def run_app(self):
-        self.root.mainloop()
+        self.mainloop()
 
 
 if __name__ == '__main__':
