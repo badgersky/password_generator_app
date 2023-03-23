9aa4d323b0eeb86edfc5f57112f9178becf90ae0
badgersky <szymonek.ptasznik@gmail.com>
2023-03-23 20:38:36 +0100

    changed validation function added displaying password function

--- top_window.py
+++ top_window.py
@@ -15,13 +15,23 @@ class TopLevelWindow(ctk.CTkToplevel):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=2)
 
-        # finding password by platform name
-        text = 'Password for:'
-        self.to_what_label = ctk.CTkLabel(self, text=text)
-        self.to_what_label.grid(column=0, row=0, padx=20, pady=10, sticky='w')
+        # password
+        self.password_label = ctk.CTkLabel(self, text='Password:')
+        self.password_label.grid(column=0, row=0, padx=20, pady=10, sticky='w')
+
+        self.password_textbox = ctk.CTkTextbox(self, height=30)
+        self.password_textbox.grid(column=1, row=0, padx=20, pady=10, sticky='nsew', columnspan=2)
+
+        # password for what
+        self.to_what_label = ctk.CTkLabel(self, text='Password for:')
+        self.to_what_label.grid(column=0, row=1, padx=20, pady=10, sticky='w')
 
         self.to_what_textbox = ctk.CTkTextbox(self, height=30)
-        self.to_what_textbox.grid(column=1, row=0, padx=20, pady=10, sticky='nsew')
+        self.to_what_textbox.grid(column=1, row=1, padx=20, pady=10, sticky='nsew')
+
+        # find password
+        self.find_password_button = ctk.CTkButton(self, text='FindPassword', command=self.display_password)
+        self.find_password_button.grid(column=0, row=2, padx=20, pady=10, sticky='w')
 
     @staticmethod
     def load_from_csv():
@@ -35,12 +45,20 @@ class TopLevelWindow(ctk.CTkToplevel):
     def validate_password_for_input(self):
         password_for = self.to_what_textbox.get('0.0', 'end')
         if password_for.strip() == '':
-            messagebox.showerror('error', '"Password for:" cannot be empty')
+            self.password_textbox.delete('0.0', 'end')
+            return False
         else:
-            passwords = self.load_from_csv()
-            for password in passwords:
-                if password_for.strip() != password[0]:
-                    messagebox.showerror('error', '"There is no password for that platform"')
+            return True
+
+    def display_password(self):
+        if self.validate_password_for_input():
+            rows = self.load_from_csv()
+            password_for = self.to_what_textbox.get('0.0', 'end')
+            for row in rows:
+                if password_for.strip() == row[0]:
+                    enc_password = decrypt_password(row[1])
+                    self.password_textbox.delete('0.0', 'end')
+                    self.password_textbox.insert('0.0', text=enc_password)
 
 
 if __name__ == '__main__':
